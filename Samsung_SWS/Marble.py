bb1ffc68f6b991659a60656b9d290da5f80dd96b
woojin <toda777@naver.com>
2022-03-07 02:58:19 +1100

    Update Marble.py-2: fixed bug

--- Samsung_SWS/Marble.py
+++ Samsung_SWS/Marble.py
@@ -6,7 +6,6 @@ input = stdin.readline
 n, m = map(int, input().split())
 
 a = [list(input().strip()) for _ in range(n)]
-print(a)
 check = [[[[False] * m for _ in range(n)] for _ in range(m)] for _ in range(n)]
 dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
 q = deque()
